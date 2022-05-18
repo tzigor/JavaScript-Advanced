d693f8dbcd4aaa4632aeb8a66107df5558115fe1
igortz <igortz@mail.ru>
2022-05-18 10:25:27 +0700

    Styles added

--- Project/script.js
+++ Project/script.js
@@ -1,22 +1,23 @@
 const goods = [
     { title: 'Shirt', price: 150 },
+    { price: 150 },
     { title: 'Socks', price: 50 },
     { title: 'Jacket', price: 350 },
     { title: 'Shoes', price: 250 },
-  ];
-  
-  const renderGoodsItem = (title, price) => {
+];
+
+const renderGoodsItem = (title = 'Item', price = 0) => {
     return `
       <div class="goods-item">
         <h3>${title}</h3>
         <p>${price}</p>
       </div>
     `;
-  };
-  
-  const renderGoodsList = (list) => {
-    let goodsList = list.map(item => renderGoodsItem(item.title, item.price));
+};
+
+const renderGoodsList = (list = []) => {
+    let goodsList = list.map(item => renderGoodsItem(item.title, item.price)).join('');
     document.querySelector('.goods-list').innerHTML = goodsList;
-  }
-  
-  renderGoodsList(goods);
+}
+
+renderGoodsList(goods);
