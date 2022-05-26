d2f2ff55b455de6c40b45752cb004bd5b673f63f
igortz <igortz@mail.ru>
2022-05-26 17:51:56 +0700

    Lesson_3 HW completed

--- Project/script.js
+++ Project/script.js
@@ -1,21 +1,26 @@
-const goods = [
-    { title: 'Shirt', price: 155 },
-    { price: 150 },
-    { title: 'Hat' },
-    { title: 'Socks', price: 51 },
-    { title: 'Jacket', price: 356 },
-    { title: 'Shoes', price: 257 },
-];
+const startURL = 'https://raw.githubusercontent.com/GeekBrainsTutorial/online-store-api/master/responses/';
+const catalogData = 'catalogData.json';
+const basketData = 'getBasket.json';
+
+function requestServer(url, callback) {
+    let xhr = new XMLHttpRequest();
+    xhr.open('GET', url, true);
+    xhr.send();
+    xhr.onload = function () {
+        if (xhr.status != 200) alert(`Ошибка ${xhr.status}: ${xhr.statusText}`)
+        else callback(xhr.responseText);
+    }
+}
 
 class GoodsItem {
-    constructor(title = 'Item', price = 0) {
-        this.title = title;
+    constructor(product_name = 'Item', price = 0) {
+        this.product_name = product_name;
         this.price = price;
     }
     render() {
         return `
           <div class="goods-item">
-            <h3>${this.title}</h3>
+            <h3>${this.product_name}</h3>
             <p>$${this.price}</p>
           </div>`;
     }
@@ -23,7 +28,11 @@ class GoodsItem {
 
 class GoodsList {
     fetchData() {
-        this.list = goods;
+        requestServer(startURL + catalogData, (goods) => {
+            this.list = JSON.parse(goods);
+            goodsList.render();
+            document.querySelector('.totalPrice').textContent = 'Total price: $' + goodsList.getTotalPrice();
+        });
     }
 
     getTotalPrice() {
@@ -32,15 +41,25 @@ class GoodsList {
 
     render() {
         const goodsList = this.list.map(item => {
-            const goodsItem = new GoodsItem(item.title, item.price);
+            const goodsItem = new GoodsItem(item.product_name, item.price);
             return goodsItem.render();
         }).join('');
         document.querySelector('.goods-list').innerHTML = goodsList;
     }
 }
 
+class Basket {
+    fetchData() {
+        requestServer(startURL + basketData, (basketContent) => {
+            this.basketLst = JSON.parse(basketContent);
+            debugger
+        });
+    }
+
+}
+
+const basket = new Basket();
+basket.fetchData();
+console.log(basket.basketLst);
 const goodsList = new GoodsList();
 goodsList.fetchData();
-goodsList.render();
-console.log(goodsList.getTotalPrice());
-document.querySelector('.totalPrice').textContent = 'Total price: $' + goodsList.getTotalPrice();
